79604a88695e108e90b23b779de0faa8b97faf0d
elistratovds <elistratov04@bk.ru>
2025-05-07 21:52:42 +0300

    final version

--- PCA_main.cpp
+++ PCA_main.cpp
@@ -1,14 +1,4 @@
-
-
-
-
-
-
-
-
 #include "PCA.h"
-
-
 
 
 
@@ -29,80 +19,102 @@ int main() {
     	cin >> number_of_parametres;
     	cout << "parametres: ";
     	cin >> parametres;
-    	Matrix data(number_of_parametres, parametres);    
+    	MyMatrix data(number_of_parametres, parametres);    
    
-    	pair<vector<string>, Matrix> Pair = writeMatrixFromCSV(name_of_file, number_of_parametres, parametres);
+    	pair<vector<string>, MyMatrix> Pair = writeMatrixFromCSV(name_of_file, number_of_parametres, parametres);
     	name = Pair.first;
     	data = Pair.second;
 
 
-    	Matrix data_T = data.transposed();
-    	Matrix data_TR = data.transposed();
+    	MyMatrix data_T = data.transposed();
+    	MyMatrix data_TR = data.transposed();
 
-   	Matrix data_T_standardization =  data_T.standardization();
+   	MyMatrix data_T_standardization =  data_T.standardization();
 
-    	Matrix cov_data = data_T_standardization.covariation();
+    	MyMatrix cov_data = data_T_standardization.covariation();
 
-    	pair<Matrix, Matrix> EIG = cov_data.rotation_method(cov_data.size());
-    	Matrix eigenvalues = EIG.first;
-    	Matrix eigenvectors = EIG.second;
+    	pair<MyMatrix, MyMatrix> EIG = cov_data.rotation_method(cov_data.size());
+    	MyMatrix eigenvalues = EIG.first;
+    	MyMatrix eigenvectors = EIG.second;
+	
 
-
-
-    	Matrix eigenvectors_T = eigenvectors.transposed();
-   	Matrix final_matrix = eigenvectors_T * data_TR;
-   	final_matrix.print();
+    	MyMatrix eigenvectors_T = eigenvectors.transposed();
+   	MyMatrix final_matrix = eigenvectors_T * data_TR;
+   	
   	main_components(final_matrix, eigenvalues, name);
   
- 	Matrix  final_matrix_tr = final_matrix.transposed();
-    	writeMatrixToCSV(name, final_matrix_tr, "output1.csv");
+ 	MyMatrix  final_matrix_tr = final_matrix.transposed();
+    	writeMatrixToCSV(name, final_matrix_tr, "output.csv");
         }
     else {
-	Mat image = imread("chisto (1).jpg",IMREAD_GRAYSCALE);
-	const int blockSize = 16;
-	const int k = 64;
+	   
+    Mat image = imread("image.jpg", IMREAD_GRAYSCALE);
+    if (image.empty()) {
+        cerr << "Ошибка загрузки изображения!" << endl;
+        return 1;
+    }
+    
+    const int blockSize = 16;    
+    const int k = 32;           
+    
+    
+    Mat imageDouble;
+    image.convertTo(imageDouble, CV_64F);
+    normalize(imageDouble, imageDouble, 0, 1, NORM_MINMAX);
 
-	Mat imageDouble;
-	image.convertTo(imageDouble,CV_64F);
-	normalize(imageDouble, imageDouble, 0, 1, NORM_MINMAX);
+ 
+    vector<VectorXd> blocks = imageToBlocks(imageDouble, blockSize);
 	
-	
-	Matrix blocks = imageToBlocks(imageDouble, blockSize);
-	Matrix blocks_cnt = blocks.center(blocks, 0);
-		
-//	b.print();
-        
-
-	Matrix blocks_cov = blocks_cnt.covariation();
-//	blocks_cov.print();
-//	cout << blocks_cov.size() << endl;
-//	cout << blocks_cov.cols()<< endl;	
-	pair<Matrix, Matrix> EIG = blocks_cov.rotation_method(blocks_cov.size());
-	Matrix eigenvalues = EIG.first;
-	Matrix eigenvectors = EIG.second;
-	Matrix eigenvectors_new = eigenvectors.leftCols(k);
-//	eigenvalues.print();
-	Matrix Y = blocks_cnt * eigenvectors_new;
-        Matrix X_final = Y * eigenvectors_new.transposed();	
-	X_final = X_final.center(X_final,1);
-	
+    
+    MatrixXd X(blocks.size(), blockSize*blockSize);
+    for (size_t i = 0; i < blocks.size(); ++i) {
+        X.row(i) = blocks[i];
+    }
 
 
-
-//	main_components(final_matrix, eigenvalues, name);
-	Mat construktImage = blocksToImage(X_final, image.rows, image.cols, blockSize);
-	normalize(construktImage,construktImage,0,255,NORM_MINMAX);
-	construktImage.convertTo(construktImage,CV_8U);
-	imwrite("reconstruct_image_2.jpg",construktImage);
-	cout << "The method is done!" << endl;
-
-	
-	
-	
-	
-		
+    
+    VectorXd mean = X.colwise().mean();
+    X.rowwise() -= mean.transpose();
 
 
+   
+    MatrixXd cov = (X.adjoint() * X) / (X.rows() - 1);
+   
+    
+    SelfAdjointEigenSolver<MatrixXd> solver(cov);
+    
+    
+    MatrixXd eigenvectors = solver.eigenvectors();
+    
+    VectorXd eigenvalues = solver.eigenvalues();
+    
+    
+    eigenvectors = eigenvectors.rowwise().reverse().eval();
+    eigenvalues = eigenvalues.reverse().eval();
+
+  
+    MatrixXd V_k = eigenvectors.leftCols(k);
+
+    
+    MatrixXd Y = X * V_k;
+    MatrixXd X_reconstructed = Y * V_k.transpose();
+    X_reconstructed.rowwise() += mean.transpose();
+
+   
+    vector<VectorXd> reconstructedBlocks;
+    for (int i = 0; i < X_reconstructed.rows(); ++i) {
+        reconstructedBlocks.push_back(X_reconstructed.row(i));
+    }
+    
+    Mat reconstructedImage = blocksToImage(reconstructedBlocks, image.rows, image.cols, blockSize);
+    
+  
+    normalize(reconstructedImage, reconstructedImage, 0, 255, NORM_MINMAX);
+    reconstructedImage.convertTo(reconstructedImage, CV_8U);
+    imwrite("denoised_image.jpg", reconstructedImage);
+
+    cout << "Обработка завершена. Результат: denoised_image.jpg" << endl; 
 	}    
-return 0;
+
+	return 0;
 }
